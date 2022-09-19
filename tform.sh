beba9ba7a95ded2669ca32f9d5e9ff2be11d6ea9
shashn-fortanix <111429685+shashn-fortanix@users.noreply.github.com>
2022-09-19 09:20:19 +0530

    Update tform.sh

--- tform.sh
+++ tform.sh
@@ -1,5 +1,8 @@
 go get
 make install
 ls
-cd ~/terraform-provider-dsm
+
+cd ~/var/lib/jenkins
+ls
+# cd ~/terraform-provider-dsm
 # find ./terraform-provider-dsm -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum
