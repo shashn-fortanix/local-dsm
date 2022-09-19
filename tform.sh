ae98b9e0acec6e7a0aba5734bfec669e9a8e19b3
shashn-fortanix <111429685+shashn-fortanix@users.noreply.github.com>
2022-09-19 13:25:28 +0530

    Update tform.sh

--- tform.sh
+++ tform.sh
@@ -1,12 +1,7 @@
+pwd
 go get
 make install
 pwd
-
-ls
-cd ~/workspace/jenkintegration
-pwd
-ls
-echo "sha1sum is"
 # cd ~/terraform-provider-dsm
 # find ~/jenkintegration -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum
-sha1sum ~/workspace/jenkintegration/* | sha1sum
+# sha1sum ~/workspace/jenkintegration/* | sha1sum
