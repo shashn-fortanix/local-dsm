997d1a53088129602b5294d75b2dcadafaa81ad2
shashn-fortanix <111429685+shashn-fortanix@users.noreply.github.com>
2022-09-19 13:13:51 +0530

    Update setupsq.sh

--- setupsq.sh
+++ setupsq.sh
@@ -4,10 +4,10 @@
 
 export FORTANIX_API_ENDPOINT="https://sdkms.fortanix.com/"
 export FORTANIX_API_KEY="MzRkYmYwNTItZmY5ZC00MTEwLWFkZTUtYWQ3MWRmNzU2YWQ1OmdQM1pSYWZrOFFkcUkyR1FrZG80SUp6ZU9kRGhjTVNqdDlhT3M5bVZ3VURuNXlWNWpyYzh4TEozeVZGUVE1NU5PcmdZVEltRGJSSE93T2ROS0NwT2RR"
-sq-dsm --force key generate --dsm-key="signerJ1" --userid="shashidhar.naraparaju@fortanix.com"
-sq-dsm --force key extract-cert --dsm-key="signerJ1" > signerJ.asc
+# sq-dsm --force key generate --dsm-key="signerJ1" --userid="shashidhar.naraparaju@fortanix.com"
+# sq-dsm --force key extract-cert --dsm-key="signerJ1" > signerJ.asc
  
 find ~/workspace/jenkintegration -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum > hash
 
-sq-dsm sign --dsm-key="signerJ" hash > hash.sign
-sq-dsm verify --signer-cert="signer.asc" hash.sign
+sq-dsm sign --dsm-key="signerJ1" hash > hash.sign
+sq-dsm verify --signer-cert="signerJ.asc" hash.sign
