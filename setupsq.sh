657c380cd702454fef8eaff3f1537dd1b76fd8b4
shashn-fortanix <111429685+shashn-fortanix@users.noreply.github.com>
2022-09-19 13:11:10 +0530

    Update setupsq.sh

--- setupsq.sh
+++ setupsq.sh
@@ -2,8 +2,8 @@
 # dpkg -i sq-dsm_1.0.1_amd64.deb
 # cd pgp-sq
 
-echo FORTANIX_API_ENDPOINT="https://sdkms.fortanix.com/"
-echo FORTANIX_API_KEY="MzRkYmYwNTItZmY5ZC00MTEwLWFkZTUtYWQ3MWRmNzU2YWQ1OmdQM1pSYWZrOFFkcUkyR1FrZG80SUp6ZU9kRGhjTVNqdDlhT3M5bVZ3VURuNXlWNWpyYzh4TEozeVZGUVE1NU5PcmdZVEltRGJSSE93T2ROS0NwT2RR"
+export FORTANIX_API_ENDPOINT="https://sdkms.fortanix.com/"
+export FORTANIX_API_KEY="MzRkYmYwNTItZmY5ZC00MTEwLWFkZTUtYWQ3MWRmNzU2YWQ1OmdQM1pSYWZrOFFkcUkyR1FrZG80SUp6ZU9kRGhjTVNqdDlhT3M5bVZ3VURuNXlWNWpyYzh4TEozeVZGUVE1NU5PcmdZVEltRGJSSE93T2ROS0NwT2RR"
 sq-dsm --force key generate --dsm-key="signerJ1" --userid="shashidhar.naraparaju@fortanix.com"
 sq-dsm --force key extract-cert --dsm-key="signerJ1" > signerJ.asc
  
